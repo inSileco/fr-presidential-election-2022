a5b8feccec7b5c86cb4fe0d2c03a60260a23785e
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-04-24 14:39:48 +0200

    Code to scrap 2nd round

--- R/get_election_results.R
+++ R/get_election_results.R
@@ -1,7 +1,5 @@
 #' Download election results from Minister of Interior
 #'
-#' Works well for first round...
-#'
 #' @param year The election year in the form of YYYY.
 #'
 #' @return No return value.
@@ -26,7 +24,9 @@ get_election_results <- function(year) {
   
   pages <- pages[-c(1, nrow(pages)), ]
   
-  datas <- data.frame()
+  datas <- list()
+  datas[[1]] <- data.frame()
+  datas[[2]] <- data.frame()
   
   for (i in 1:nrow(pages)) {
     
@@ -35,40 +35,53 @@ get_election_results <- function(year) {
     
     page <- rvest::session(paste0(url, "/", pages[i, "url"]))
     
-    tables <- rvest::html_table(page, dec = ",")[-1]
+    tables <- rvest::html_table(page, dec = ",")
+    tables <- tables[-1]
     
-    candidates <- as.data.frame(tables[[1]])
-    candidates[ , 2] <- as.numeric(gsub("\\s", "", candidates[ , 2]))
-    
-    resume <- as.data.frame(tables[[2]])
-    resume[ , 2] <- as.numeric(gsub("\\s", "", resume[ , 2]))
-    
-    dat <- data.frame(
-      `Code`        = code_dpt, 
-      `Departement` = libelle_dpt,
-      `Inscrits`    = resume[resume[ , 1] == "Inscrits", "Nombre"],
-      `Abstentions` = resume[resume[ , 1] == "Abstentions", "Nombre"],
-      `Votants`     = resume[resume[ , 1] == "Votants", "Nombre"],
-      `Blancs`      = resume[resume[ , 1] == "Blancs", "Nombre"],
-      `Nuls`        = resume[resume[ , 1] == "Nuls", "Nombre"],
-      `Exprimes`    = resume[resume[ , 1] == "Exprimés", "Nombre"])
-    
-    for (j in 1:nrow(candidates)) {
+    for (round in 1:2) {
       
-      info <- strsplit(candidates[j, 1], "\\s")[[1]]
-      name <- info[-c(1:2)]
-      
-      dat <- data.frame(dat, "Sexe" = ifelse(info[1] == "M.", "M", "F"))
-      dat <- data.frame(dat, "Nom"  = paste0(name, collapse = " "))
-      dat <- data.frame(dat, "Voix" = candidates[j, "Voix"])
+      if (round == 1 || length(tables) == 4 && round == 2) {
+        
+        candidates <- as.data.frame(tables[[round + (round - 1)]])
+        candidates[ , 2] <- as.numeric(gsub("\\s", "", candidates[ , 2]))
+        
+        resume <- as.data.frame(tables[[round + (round - 1) + 1]])
+        resume[ , 2] <- as.numeric(gsub("\\s", "", resume[ , 2]))
+        
+        dat <- data.frame(
+          `Code`        = code_dpt, 
+          `Departement` = libelle_dpt,
+          `Inscrits`    = resume[resume[ , 1] == "Inscrits", "Nombre"],
+          `Abstentions` = resume[resume[ , 1] == "Abstentions", "Nombre"],
+          `Votants`     = resume[resume[ , 1] == "Votants", "Nombre"],
+          `Blancs`      = resume[resume[ , 1] == "Blancs", "Nombre"],
+          `Nuls`        = resume[resume[ , 1] == "Nuls", "Nombre"],
+          `Exprimes`    = resume[resume[ , 1] == "Exprimés", "Nombre"])
+        
+        for (j in 1:nrow(candidates)) {
+          
+          info <- strsplit(candidates[j, 1], "\\s")[[1]]
+          name <- info[-c(1:2)]
+          
+          dat <- data.frame(dat, "Sexe" = ifelse(info[1] == "M.", "M", "F"))
+          dat <- data.frame(dat, "Nom"  = paste0(name, collapse = " "))
+          dat <- data.frame(dat, "Voix" = candidates[j, "Voix"])
+        }
+        
+        datas[[round]] <- rbind(datas[[round]], dat)
+      }
     }
-    
-    datas <- rbind(datas, dat)
-  } 
+  }
   
-  write.csv(datas, here::here("data", "raw-data", 
-                              "Presidentielle_", year,"_Resultats_Tour_1_c.csv"),
-            row.names = FALSE)
+  for (round in 1:2) {
+    if (nrow(datas[[round]])) {
+      write.csv(datas[[round]], 
+                here::here("data", "raw-data", 
+                           paste0("Presidentielle_", year,"_Resultats_Tour_", 
+                                  round, "_c.csv")),
+                row.names = FALSE) 
+    }
+  }
   
   invisible(NULL)
 }
